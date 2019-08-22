1095871b7e9780b2ed1348333ce7906fad823b50
Hongyu Wang <124439607@qq.com>
2019-08-22 11:19:01 +0800

    Update SessionForm.Designer.cs

--- GUI/SessionForm.Designer.cs
+++ GUI/SessionForm.Designer.cs
@@ -96,7 +96,7 @@
             this.mPacketContextNameLabel.Enabled = false;
             this.mPacketContextNameLabel.Name = "mPacketContextNameLabel";
             this.mPacketContextNameLabel.Size = new System.Drawing.Size(245, 22);
-            this.mPacketContextNameLabel.Text = "Name:";
+            this.mPacketContextNameLabel.Text = "封包类型名称:";
             // 
             // mPacketContextNameBox
             // 
@@ -115,7 +115,7 @@
             this.mPacketContextIgnoreMenu.CheckOnClick = true;
             this.mPacketContextIgnoreMenu.Name = "mPacketContextIgnoreMenu";
             this.mPacketContextIgnoreMenu.Size = new System.Drawing.Size(245, 22);
-            this.mPacketContextIgnoreMenu.Text = "Ignore";
+            this.mPacketContextIgnoreMenu.Text = "忽略该类封包";
             this.mPacketContextIgnoreMenu.CheckedChanged += new System.EventHandler(this.mPacketContextIgnoreMenu_CheckedChanged);
             // 
             // removePacketFromSessionToolStripMenuItem
@@ -127,13 +127,13 @@
             this.allAfterThisOneToolStripMenuItem});
             this.removePacketFromSessionToolStripMenuItem.Name = "removePacketFromSessionToolStripMenuItem";
             this.removePacketFromSessionToolStripMenuItem.Size = new System.Drawing.Size(245, 22);
-            this.removePacketFromSessionToolStripMenuItem.Text = "Remove packet from session";
+            this.removePacketFromSessionToolStripMenuItem.Text = "从会话中移除封包";
             // 
             // thisPacketOnlyToolStripMenuItem
             // 
             this.thisPacketOnlyToolStripMenuItem.Name = "thisPacketOnlyToolStripMenuItem";
             this.thisPacketOnlyToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
-            this.thisPacketOnlyToolStripMenuItem.Text = "This packet only";
+            this.thisPacketOnlyToolStripMenuItem.Text = "仅该封包;
             this.thisPacketOnlyToolStripMenuItem.Click += new System.EventHandler(this.thisPacketOnlyToolStripMenuItem_Click);
             // 
             // toolStripSeparator2
@@ -148,21 +148,21 @@
             this.allToolStripMenuItem});
             this.allBeforeThisOneToolStripMenuItem.Name = "allBeforeThisOneToolStripMenuItem";
             this.allBeforeThisOneToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
-            this.allBeforeThisOneToolStripMenuItem.Text = "Every packet before this one";
+            this.allBeforeThisOneToolStripMenuItem.Text = "之前所有封包";
             this.allBeforeThisOneToolStripMenuItem.Click += new System.EventHandler(this.allBeforeThisOneToolStripMenuItem_Click);
             // 
             // onlyVisibleToolStripMenuItem
             // 
             this.onlyVisibleToolStripMenuItem.Name = "onlyVisibleToolStripMenuItem";
             this.onlyVisibleToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
-            this.onlyVisibleToolStripMenuItem.Text = "Only visible";
+            this.onlyVisibleToolStripMenuItem.Text = "仅列出可见";
             this.onlyVisibleToolStripMenuItem.Click += new System.EventHandler(this.onlyVisibleToolStripMenuItem_Click);
             // 
             // allToolStripMenuItem
             // 
             this.allToolStripMenuItem.Name = "allToolStripMenuItem";
             this.allToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
-            this.allToolStripMenuItem.Text = "All";
+            this.allToolStripMenuItem.Text = "所有";
             this.allToolStripMenuItem.Click += new System.EventHandler(this.allToolStripMenuItem_Click);
             // 
             // allAfterThisOneToolStripMenuItem
@@ -172,20 +172,20 @@
             this.allToolStripMenuItem1});
             this.allAfterThisOneToolStripMenuItem.Name = "allAfterThisOneToolStripMenuItem";
             this.allAfterThisOneToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
-            this.allAfterThisOneToolStripMenuItem.Text = "Every packet after this one";
+            this.allAfterThisOneToolStripMenuItem.Text = "之后所有封包";
             // 
             // onlyVisibleToolStripMenuItem1
             // 
             this.onlyVisibleToolStripMenuItem1.Name = "onlyVisibleToolStripMenuItem1";
             this.onlyVisibleToolStripMenuItem1.Size = new System.Drawing.Size(142, 22);
-            this.onlyVisibleToolStripMenuItem1.Text = "Only visible";
+            this.onlyVisibleToolStripMenuItem1.Text = "仅列出可见";
             this.onlyVisibleToolStripMenuItem1.Click += new System.EventHandler(this.onlyVisibleToolStripMenuItem1_Click);
             // 
             // allToolStripMenuItem1
             // 
             this.allToolStripMenuItem1.Name = "allToolStripMenuItem1";
             this.allToolStripMenuItem1.Size = new System.Drawing.Size(142, 22);
-            this.allToolStripMenuItem1.Text = "All";
+            this.allToolStripMenuItem1.Text = "所有";
             this.allToolStripMenuItem1.Click += new System.EventHandler(this.allToolStripMenuItem1_Click);
             // 
             // mMenu
@@ -213,7 +213,7 @@
             this.mMainFileMenu.MergeAction = System.Windows.Forms.MergeAction.MatchOnly;
             this.mMainFileMenu.Name = "mMainFileMenu";
             this.mMainFileMenu.Size = new System.Drawing.Size(39, 21);
-            this.mMainFileMenu.Text = "&File";
+            this.mMainFileMenu.Text = "文件";
             // 
             // mFileSeparatorMenu
             // 
@@ -230,7 +230,7 @@
             this.mFileSaveMenu.Name = "mFileSaveMenu";
             this.mFileSaveMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
             this.mFileSaveMenu.Size = new System.Drawing.Size(158, 22);
-            this.mFileSaveMenu.Text = "&Save";
+            this.mFileSaveMenu.Text = "保存";
             this.mFileSaveMenu.Click += new System.EventHandler(this.MFileSaveMenu_Click);
             // 
             // mFileExportMenu
@@ -240,7 +240,7 @@
             this.mFileExportMenu.Name = "mFileExportMenu";
             this.mFileExportMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
             this.mFileExportMenu.Size = new System.Drawing.Size(158, 22);
-            this.mFileExportMenu.Text = "&Export";
+            this.mFileExportMenu.Text = "导出";
             this.mFileExportMenu.Click += new System.EventHandler(this.mFileExportMenu_Click);
             // 
             // mViewMenu
@@ -261,7 +261,7 @@
             this.mViewMenu.MergeAction = System.Windows.Forms.MergeAction.MatchOnly;
             this.mViewMenu.Name = "mViewMenu";
             this.mViewMenu.Size = new System.Drawing.Size(47, 21);
-            this.mViewMenu.Text = "&View";
+            this.mViewMenu.Text = "视图";
             // 
             // mViewCommonScriptMenu
             // 
@@ -270,7 +270,7 @@
             this.mViewCommonScriptMenu.Name = "mViewCommonScriptMenu";
             this.mViewCommonScriptMenu.ShortcutKeys = System.Windows.Forms.Keys.F1;
             this.mViewCommonScriptMenu.Size = new System.Drawing.Size(243, 22);
-            this.mViewCommonScriptMenu.Text = "Common Script";
+            this.mViewCommonScriptMenu.Text = "通用脚本";
             this.mViewCommonScriptMenu.Click += new System.EventHandler(this.mViewCommonScriptMenu_Click);
             // 
             // mViewSeparator1Menu
@@ -287,7 +287,7 @@
             this.mViewRefreshMenu.Name = "mViewRefreshMenu";
             this.mViewRefreshMenu.ShortcutKeys = System.Windows.Forms.Keys.F5;
             this.mViewRefreshMenu.Size = new System.Drawing.Size(243, 22);
-            this.mViewRefreshMenu.Text = "&Refresh";
+            this.mViewRefreshMenu.Text = "刷新";
             this.mViewRefreshMenu.Click += new System.EventHandler(this.mViewRefreshMenu_Click);
             // 
             // mViewSeparator2Menu
@@ -331,7 +331,7 @@
             this.mViewIgnoredMenu.Name = "mViewIgnoredMenu";
             this.mViewIgnoredMenu.ShortcutKeys = System.Windows.Forms.Keys.F8;
             this.mViewIgnoredMenu.Size = new System.Drawing.Size(243, 22);
-            this.mViewIgnoredMenu.Text = "Ignored";
+            this.mViewIgnoredMenu.Text = "已忽略封包";
             this.mViewIgnoredMenu.CheckedChanged += new System.EventHandler(this.mViewIgnoredMenu_CheckedChanged);
             // 
             // mViewSeparator3Menu
@@ -346,7 +346,7 @@
             this.sessionInformationToolStripMenuItem.Name = "sessionInformationToolStripMenuItem";
             this.sessionInformationToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.I)));
             this.sessionInformationToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
-            this.sessionInformationToolStripMenuItem.Text = "Session Information";
+            this.sessionInformationToolStripMenuItem.Text = "会话信息";
             this.sessionInformationToolStripMenuItem.Click += new System.EventHandler(this.sessionInformationToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
@@ -359,7 +359,7 @@
             this.sendpropertiesToolStripMenuItem.Name = "sendpropertiesToolStripMenuItem";
             this.sendpropertiesToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.D1)));
             this.sendpropertiesToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
-            this.sendpropertiesToolStripMenuItem.Text = "Open send.properties";
+            this.sendpropertiesToolStripMenuItem.Text = "打开 send.properties";
             this.sendpropertiesToolStripMenuItem.Click += new System.EventHandler(this.sendpropertiesToolStripMenuItem_Click);
             // 
             // recvpropertiesToolStripMenuItem
@@ -367,7 +367,7 @@
             this.recvpropertiesToolStripMenuItem.Name = "recvpropertiesToolStripMenuItem";
             this.recvpropertiesToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.D2)));
             this.recvpropertiesToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
-            this.recvpropertiesToolStripMenuItem.Text = "Open recv.properties";
+            this.recvpropertiesToolStripMenuItem.Text = "打开 recv.properties";
             this.recvpropertiesToolStripMenuItem.Click += new System.EventHandler(this.recvpropertiesToolStripMenuItem_Click);
             // 
             // dataToolStripMenuItem
@@ -376,20 +376,20 @@
             this.removeLoggedPacketsToolStripMenuItem});
             this.dataToolStripMenuItem.Name = "dataToolStripMenuItem";
             this.dataToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
-            this.dataToolStripMenuItem.Text = "Data";
+            this.dataToolStripMenuItem.Text = "数据";
             // 
             // removeLoggedPacketsToolStripMenuItem
             // 
             this.removeLoggedPacketsToolStripMenuItem.Name = "removeLoggedPacketsToolStripMenuItem";
             this.removeLoggedPacketsToolStripMenuItem.Size = new System.Drawing.Size(218, 22);
-            this.removeLoggedPacketsToolStripMenuItem.Text = "Remove logged packets";
+            this.removeLoggedPacketsToolStripMenuItem.Text = "移除已记录封包";
             this.removeLoggedPacketsToolStripMenuItem.Click += new System.EventHandler(this.removeLoggedPacketsToolStripMenuItem_Click);
             // 
             // addPacketsToolStripMenuItem
             // 
             this.addPacketsToolStripMenuItem.Name = "addPacketsToolStripMenuItem";
             this.addPacketsToolStripMenuItem.Size = new System.Drawing.Size(93, 21);
-            this.addPacketsToolStripMenuItem.Text = "Add packets";
+            this.addPacketsToolStripMenuItem.Text = "添加封包";
             this.addPacketsToolStripMenuItem.Click += new System.EventHandler(this.addPacketsToolStripMenuItem_Click_1);
             // 
             // 忽略列表ToolStripMenuItem
@@ -403,13 +403,13 @@
             // 
             this.mSaveDialog.Filter = "MapleShark Binary Files|*.msb";
             this.mSaveDialog.RestoreDirectory = true;
-            this.mSaveDialog.Title = "Save";
+            this.mSaveDialog.Title = "保存";
             // 
             // mExportDialog
             // 
             this.mExportDialog.Filter = "Text Files|*.txt";
             this.mExportDialog.RestoreDirectory = true;
-            this.mExportDialog.Title = "Export";
+            this.mExportDialog.Title = "导出";
             // 
             // mPacketList
             // 
@@ -437,26 +437,26 @@
             // 
             // mTimestampColumn
             // 
-            this.mTimestampColumn.Text = "Timestamp";
+            this.mTimestampColumn.Text = "时间戳";
             this.mTimestampColumn.Width = 120;
             // 
             // mDirectionColumn
             // 
-            this.mDirectionColumn.Text = "Direction";
+            this.mDirectionColumn.Text = "方向";
             this.mDirectionColumn.Width = 75;
             // 
             // mLengthColumn
             // 
-            this.mLengthColumn.Text = "Length";
+            this.mLengthColumn.Text = "长度";
             this.mLengthColumn.Width = 64;
             // 
             // mOpcodeColumn
             // 
-            this.mOpcodeColumn.Text = "Opcode";
+            this.mOpcodeColumn.Text = "封包头";
             // 
             // mNameColumn
             // 
-            this.mNameColumn.Text = "Name";
+            this.mNameColumn.Text = "封包类型名称";
             this.mNameColumn.Width = 175;
             // 
             // 复制数据到剪贴板ToolStripMenuItem
@@ -480,7 +480,7 @@
             this.Name = "SessionForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.Text = "SessionForm";
+            this.Text = "会话表格";
             this.Load += new System.EventHandler(this.SessionForm_Load);
             this.mPacketContextMenu.ResumeLayout(false);
             this.mPacketContextMenu.PerformLayout();
